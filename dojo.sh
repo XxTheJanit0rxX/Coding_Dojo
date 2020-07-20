05c4cb023b8a87574692571b50a4345a34f1dfd8
XxTheJanit0rxX <39220678+XxTheJanit0rxX@users.noreply.github.com>
2020-07-20 21:01:38 +0000

    Add files via upload

---
+++ dojo.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+cd $HOME/Desktop
+#Create HTML folders and add Index.html
+mkdir -p Dojo_Assignments/WebFundamentals/HTML/Indentation_Practice
+mkdir -p Dojo_Assignments/WebFundamentals/HTML/Registration_form
+mkdir -p Dojo_Assignments/WebFundamentals/HTML/Fake_Blog
+mkdir -p Dojo_Assignments/WebFundamentals/HTML/Algorithm_I
+touch Dojo_Assignments/WebFundamentals/HTML/Indentation_Practice/index.html
+touch Dojo_Assignments/WebFundamentals/HTML/Registration_form/index.html
+touch Dojo_Assignments/WebFundamentals/HTML/Fake_Blog/index.html
+touch Dojo_Assignments/WebFundamentals/HTML/Algorithm_I/index.html
+#Create CSS folder and add index.html and style.css
+mkdir -p Dojo_Assignments/WebFundamentals/CSS/Plotting_your_Blocks
+mkdir -p Dojo_Assignments/WebFundamentals/CSS/Javascript._Basics
+mkdir -p Dojo_Assignments/WebFundamentals/CSS/Portfolio
+mkdir -p Dojo_Assignments/WebFundamentals/CSS/Python_Basics
+touch Dojo_Assignments/WebFundamentals/CSS/Plotting_your_Blocks/index.html
+touch Dojo_Assignments/WebFundamentals/CSS/Plotting_your_Blocks/style.css
+touch Dojo_Assignments/WebFundamentals/CSS/Javascript._Basics/index.html
+touch Dojo_Assignments/WebFundamentals/CSS/Javascript._Basics/style.css
+touch Dojo_Assignments/WebFundamentals/CSS/Portfolio/style.css
+touch Dojo_Assignments/WebFundamentals/CSS/Portfolio/index.html
+touch Dojo_Assignments/WebFundamentals/CSS/Python_Basics/index.html
+touch Dojo_Assignments/WebFundamentals/CSS/Python_Basics/style.css
+#Create Javascript folder and subfolders
+mkdir -p Dojo_Assignments/WebFundamentals/Javascript/Basic_Function_I
+mkdir -p Dojo_Assignments/WebFundamentals/Javascript/Basic_Foundation_I
+mkdir -p Dojo_Assignments/WebFundamentals/Javascript/Basic_Foundation_II
+mkdir -p Dojo_Assignments/WebFundamentals/Javascript/Intermediate_Foundation
+touch Dojo_Assignments/WebFundamentals/Javascript/Basic_Function_I/index.html
+touch Dojo_Assignments/WebFundamentals/Javascript/Basic_Function_I/style.css
+touch Dojo_Assignments/WebFundamentals/Javascript/Basic_Foundation_I/index.html
+touch Dojo_Assignments/WebFundamentals/Javascript/Basic_Foundation_I/style.css
+touch Dojo_Assignments/WebFundamentals/Javascript/Basic_Foundation_II/index.html
+touch Dojo_Assignments/WebFundamentals/Javascript/Basic_Foundation_II/style.css
+touch Dojo_Assignments/WebFundamentals/Javascript/Intermediate_Foundation/style.css
+touch Dojo_Assignments/WebFundamentals/Javascript/Intermediate_Foundation/index.html
+#Create Jquery folder and subfolders
+mkdir -p Dojo_Assignments/WebFundamentals/Jquery/jquery_Function
+mkdir -p Dojo_Assignments/WebFundamentals/Jquery/Disappearing_Ninja
+mkdir -p Dojo_Assignments/WebFundamentals/Jquery/Ninja_to_Cat
+mkdir -p Dojo_Assignments/WebFundamentals/Jquery/Debugging_Jquery
+mkdir -p Dojo_Assignments/WebFundamentals/Jquery/Hover_in:out
+touch Dojo_Assignments/WebFundamentals/Jquery/jquery_Function/index.html
+touch Dojo_Assignments/WebFundamentals/Jquery/jquery_Function/style.css
+touch Dojo_Assignments/WebFundamentals/Jquery/jquery_Function/script.js
+touch Dojo_Assignments/WebFundamentals/Jquery/Disappearing_Ninja/index.html
+touch Dojo_Assignments/WebFundamentals/Jquery/Disappearing_Ninja/style.css
+touch Dojo_Assignments/WebFundamentals/Jquery/Disappearing_Ninja/script.js
+touch Dojo_Assignments/WebFundamentals/Jquery/Ninja_to_Cat/index.html
+touch Dojo_Assignments/WebFundamentals/Jquery/Ninja_to_Cat/style.css
+touch Dojo_Assignments/WebFundamentals/Jquery/Ninja_to_Cat/script.js
+touch Dojo_Assignments/WebFundamentals/Jquery/Debugging_Jquery/index.html
+touch Dojo_Assignments/WebFundamentals/Jquery/Debugging_Jquery/style.css
+touch Dojo_Assignments/WebFundamentals/Jquery/Debugging_Jquery/script.js
+touch Dojo_Assignments/WebFundamentals/Jquery/Hover_in:out/index.html
+touch Dojo_Assignments/WebFundamentals/Jquery/Hover_in:out/style.css
+#Create API folder and subfolders
+mkdir -p Dojo_Assignments/WebFundamentals/"API's"/Pokemon
+touch Dojo_Assignments/WebFundamentals/"API's"/Pokemon/index.html
+touch Dojo_Assignments/WebFundamentals/"API's"/Pokemon/style.css
+#Create Python directory and subfolders
+mkdir -p Dojo_Assignments/Python/Python._Syntax
+mkdir -p Dojo_Assignments/Python/OOP
+mkdir -p Dojo_Assignments/Python/Django
+mkdir -p Dojo_Assignments/Python/ORM
+mkdir -p Dojo_Assignments/Python/Full._Stack
+#Create MERN directory
+mkdir -p Dojo_Assignments/MERN
+#Create Java C# directory
+mkdir -p Dojo_Assignments/Java_:_C#
